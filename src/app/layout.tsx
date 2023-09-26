62bf3178a1b66ae6bf099dab98de9390227e8ba4
chen <suchen@megaease.com>
2023-09-26 18:44:51 +0800

    fix bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,13 @@ import Typography from '@mui/material/Typography';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon'
+import { ButtonBase } from '@mui/material'
+
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import FormatLineSpacingIcon from '@mui/icons-material/FormatLineSpacing';
+import BuildIcon from '@mui/icons-material/Build';
 
 const defaultCluster: ClusterType = {
   id: 0,
@@ -39,16 +46,12 @@ function SideBar() {
   const intl = useIntl()
 
   const items = [
-    { name: intl.formatMessage({ id: 'app.cluster' }), url: '/cluster' },
-    { name: intl.formatMessage({ id: 'app.pipeline' }), url: '/pipeline' },
-    { name: intl.formatMessage({ id: 'app.traffic' }), url: '/traffic' },
-    { name: intl.formatMessage({ id: 'app.controller' }), url: '/controller' },
+    { name: intl.formatMessage({ id: 'app.cluster' }), url: '/cluster', icon: <FormatListBulletedIcon /> },
+    { name: intl.formatMessage({ id: 'app.traffic' }), url: '/traffic', icon: <FilterAltIcon /> },
+    { name: intl.formatMessage({ id: 'app.pipeline' }), url: '/pipeline', icon: <FormatLineSpacingIcon /> },
+    { name: intl.formatMessage({ id: 'app.controller' }), url: '/controller', icon: <BuildIcon style={{ transform: 'scaleX(-1)' }} /> },
   ]
   const router = useRouter()
-
-  const click = () => {
-    console.log("click")
-  }
 
   return (
     <Drawer
@@ -65,6 +68,9 @@ function SideBar() {
           {items.map((item, index) => (
             <ListItem key={index} disablePadding>
               <ListItemButton onClick={() => { router.push(item.url) }}>
+                <ListItemIcon style={{ minWidth: 40 }}>
+                  {item.icon}
+                </ListItemIcon>
                 <ListItemText primary={item.name} />
               </ListItemButton>
             </ListItem>
@@ -75,11 +81,9 @@ function SideBar() {
   )
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children, }: { children: React.ReactNode }) {
+  const router = useRouter()
+
   // TODO: load clusters from file or local storage.
   const [clusters, setClusters] = React.useState<ClusterType[]>([defaultCluster])
   const [currentCluster, setCurrentCluster] = React.useState<ClusterType>(defaultCluster)
@@ -93,29 +97,33 @@ export default function RootLayout({
   return (
     <html>
       <Header />
-      <IntlProvider
-        key={'en-US'}
-        locale={'en-US'}
-        messages={translations['en-US']}
-      >
-        <Box sx={{ display: 'flex' }}>
-          <CssBaseline />
-          <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-            <Toolbar>
-              <Typography variant="h6" noWrap component="div">
-                Easegress
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <SideBar />
-          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-            <Toolbar />
-            <ClusterContext.Provider value={clusterContext}>
-              {children}
-            </ClusterContext.Provider>
+      <body>
+        <IntlProvider
+          key={'en-US'}
+          locale={'en-US'}
+          messages={translations['en-US']}
+        >
+          <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+              <Toolbar>
+                <ButtonBase onClick={() => { router.push('/') }}>
+                  <Typography variant="h6" noWrap component="div" >
+                    Easegress
+                  </Typography>
+                </ButtonBase>
+              </Toolbar>
+            </AppBar>
+            <SideBar />
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+              <Toolbar />
+              <ClusterContext.Provider value={clusterContext}>
+                {children}
+              </ClusterContext.Provider>
+            </Box>
           </Box>
-        </Box>
-      </IntlProvider>
-    </html>
+        </IntlProvider>
+      </body>
+    </html >
   )
 }
