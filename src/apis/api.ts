78a6bf2ba40a84a5d07d0493eeb5be33b324ef84
Yun Long <longdeqidao@gmail.com>
2023-10-11 23:34:48 +0800

    Add egctlConfigToAxiosInstance

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// TODO: Delete it by using axiosinstance created by egctlrc.
 // may add interceptors later
 const api = axios.create();
 export default api;
--- src/apis/cluster.ts
+++ src/apis/cluster.ts
@@ -1,7 +1,6 @@
 import api from "./api"
-import { AxiosRequestConfig } from "axios"
 import { urls } from "./urls"
-import { AxiosResponse } from "axios"
+import axios, { AxiosResponse, AxiosRequestConfig, AxiosInstance, Axios } from "axios"
 
 export type ClusterType = {
   id: number
@@ -55,6 +54,52 @@ type EgctlConfig = {
 
   'current-context': string
 }
+
+function egctlConfigToAxiosInstance(config: EgctlConfig): AxiosInstance {
+  const axiosConfig: AxiosRequestConfig = {
+    headers: {
+      'Accept': 'application/json',
+    },
+    responseType: 'json'
+  };
+
+  var ctx = config.contexts.find(ctx => ctx.name === config['current-context'])
+  var user = config.users.find(user => user.name === ctx?.context.user)
+  var cluster = config.clusters.find(cluster => cluster.name === ctx?.context.cluster)
+
+  var enableMTLS = false
+  if (cluster?.cluster["certificate-authority-data"] !== "") {
+    enableMTLS = true
+  }
+
+  var url = cluster?.cluster.server
+  if (enableMTLS) {
+    url = "https://" + url
+  }
+  axiosConfig.url = url
+
+  if (enableMTLS) {
+    axiosConfig.httpsAgent = new (require('https').Agent)({
+      rejectUnauthorized: true,
+      cert: user?.user['client-certificate-data'],
+      key: user?.user['client-key-data'],
+      ca: cluster?.cluster['certificate-authority-data'],
+    })
+  }
+
+  if (user?.user['username'] !== "") {
+    axiosConfig.auth = {
+      username: user?.user['username'] || "",
+      password: user?.user['password'] || "",
+    }
+  }
+
+  const instance = axios.create(axiosConfig);
+
+  return instance
+}
+
+
 
 export function getClientInfo(cluster: ClusterType, path: string): ClientInfo {
   // TODO: later may check protocol, and add auth, mtls to config
